25f8f862863d8291a0e599f48d4f8b85f4854807
UNSynaps <78422678+UNSynaps@users.noreply.github.com>
2026-03-30 12:23:22 -0500

    Update build.gradle.kts

--- Pelisplus4KProvider/build.gradle.kts
+++ Pelisplus4KProvider/build.gradle.kts
@@ -26,3 +26,8 @@ cloudstream {
 
     iconUrl = "https://www.google.com/s2/favicons?domain=pelisplus.to&sz=%size%"
 }
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile> {
+    compilerOptions {
+        freeCompilerArgs.add("-Xsuppress-warnings")
+    }
+}
