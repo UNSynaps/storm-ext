586e3532ce38b599cf5a584b73e0607a019653e9
UNSynaps <78422678+UNSynaps@users.noreply.github.com>
2026-03-30 13:31:09 -0500

    Update MonoschinosProvider.kt

--- MonoschinosProvider/src/main/kotlin/com/stormunblessed/MonoschinosProvider.kt
+++ MonoschinosProvider/src/main/kotlin/com/stormunblessed/MonoschinosProvider.kt
@@ -76,7 +76,7 @@ class MonoschinosProvider : MainAPI() {
                         }, isHorizontal)
         )
 
-        urls.apmap { (url, name) ->
+        urls.amap { (url, name) ->
 
             val home = app.get(url, timeout = 120).document.select("li.col").map {
                 val title = it.selectFirst("h3")!!.text()
@@ -94,7 +94,7 @@ class MonoschinosProvider : MainAPI() {
         }
 
         if (items.size <= 0) throw ErrorLoadingException()
-        return HomePageResponse(items)
+        return newHomePageResponse(items)
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
@@ -187,7 +187,7 @@ class MonoschinosProvider : MainAPI() {
             subtitleCallback: (SubtitleFile) -> Unit,
             callback: (ExtractorLink) -> Unit
     ): Boolean {
-        app.get(data).document.select("#myTab li").apmap {
+        app.get(data).document.select("#myTab li").amap {
             val encodedurl = it.select(".play-video").attr("data-player")
             val urlDecoded = base64Decode(encodedurl)
             val url = (urlDecoded).replace("https://monoschinos2.com/reproductor?url=", "")
