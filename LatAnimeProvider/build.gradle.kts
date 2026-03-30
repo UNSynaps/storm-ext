b3fc4814e46c6a3c660fbe3ab2ff658862dfc659
UNSynaps <78422678+UNSynaps@users.noreply.github.com>
2026-03-30 13:28:53 -0500

    Update build.gradle.kts

--- LatAnimeProvider/build.gradle.kts
+++ LatAnimeProvider/build.gradle.kts
@@ -24,8 +24,3 @@ cloudstream {
 
     iconUrl = "https://latanime.org/public/img/logito.png"
 }
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile> {
-    compilerOptions {
-        freeCompilerArgs.add("-Xsuppress-warnings")
-    }
-}
