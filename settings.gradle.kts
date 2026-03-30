d008cd942a47a24bc255debe4e66d577ec3a82eb
UNSynaps <78422678+UNSynaps@users.noreply.github.com>
2026-03-30 14:37:32 -0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,44 @@
 rootProject.name = "CloudstreamPlugins"
 
-// This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
-
-val disabled = listOf<String>()
-
-File(rootDir, ".").eachDir { dir ->
-    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
-        include(dir.name)
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
 
-fun File.eachDir(block: (File) -> Unit) {
-    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
+
+rootProject.name = "storm-ext"
+
+// Esta es la parte mágica que modifiqué para ti:
+File(rootDir, "src").listFiles()?.forEach { file ->
+    if (file.isDirectory && file.name == "AnimeflvProvider") {
+        include(":${file.name}")
+        project(":${file.name}").projectDir = file
+    }
+}
+
+// This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
+
+//val disabled = listOf<String>()
+
+//File(rootDir, ".").eachDir { dir ->
+    //if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
+        //include(dir.name)
+    //}
+//}
+
+//fun File.eachDir(block: (File) -> Unit) {
+    //listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
+//}
 
 
 // To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
